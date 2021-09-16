69dc9a5174e31dd773eecd160657928bcde28ad1
petit <petitoff@outlook.com>
2021-09-16 19:43:21 +0200

    Przypisanie stylu oraz odpowiednich wyświetlanych wartości.

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,9 +1,30 @@
-import { Container, Screen } from "./Styled"
+import { Button, Container, Current, Previous, Screen } from "./Styled"
 
 export default function Calculator(){
     return(
         <Container>
-            <Screen></Screen>
+            <Screen>
+                <Previous></Previous>
+                <Current></Current>
+            </Screen>
+            <Button gridSpan={2} control>AC</Button>
+            <Button control>DEL</Button>
+            <Button operation>÷</Button>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button operation>×</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button operation>+</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button operation>-</Button>
+            <Button period>.</Button>
+            <Button>0</Button>
+            <Button operation gridSpan={2} equals>=</Button>
         </Container>
     )
 }
