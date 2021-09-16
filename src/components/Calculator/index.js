7bc7b623f489c4986a57aefa8e99ef3817d103f4
petit <petitoff@outlook.com>
2021-09-16 19:15:30 +0200

    Dodanei stylów.

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,9 @@
+import { Container, Screen } from "./Styled"
+
+export default function Calculator(){
+    return(
+        <Container>
+            <Screen></Screen>
+        </Container>
+    )
+}
