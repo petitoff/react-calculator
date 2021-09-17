1262080e3703d0369bd2a9c0d675f833a501ad93
petit <petitoff@outlook.com>
2021-09-17 12:32:32 +0200

    Dodanie działania operacji matematycznych.

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -22,6 +22,57 @@ export default function Calculator(){
         setPrevious('')
         setOperation('')
     }
+
+    const chooseOperation = (el) => {
+        if(current === '') return
+        if(current !== '' && previous !== '') return
+
+        if(previous !== ''){
+            let value = compute();
+            setPrevious(value)
+        }
+        else{
+            setPrevious(current)
+        }
+        setCurrent('')
+        setOperation(el.target.getAttribute('data'))
+    }
+
+    const equals = () => {
+        let value = compute();
+        if(value === undefined || value === null) return
+
+        setCurrent(value)
+        setPrevious('')
+        setOperation('')
+    }
+
+    const compute = () => {
+        let result
+        let previousNumber = parseFloat(previous)
+        let currentNumber = parseFloat(current)
+
+        // if(isNaN(previousNumber) || NaN(currentNumber)) return
+
+        switch(operation){
+            case '÷':
+                result = previousNumber / currentNumber;
+                break;
+            case '×':
+                result = previousNumber * currentNumber;
+                break;
+            case '+':
+                result = previousNumber + currentNumber;
+                break;
+            case '-':
+                result = previousNumber - currentNumber;
+                break;
+            default:
+                return;
+        }
+        return result
+    }
+
     return(
         <Container>
             <Screen>
@@ -30,22 +81,22 @@ export default function Calculator(){
             </Screen>
             <Button gridSpan={2} control onClick={handleAllClear}>AC</Button>
             <Button control onClick={handleDelete}>DEL</Button>
-            <Button operation>÷</Button>
+            <Button data={"÷"} onClick={chooseOperation} operation>÷</Button>
             <Button data={"7"} onClick={appendValue}>7</Button>
             <Button data={"8"} onClick={appendValue}>8</Button>
             <Button data={"9"} onClick={appendValue}>9</Button>
-            <Button operation>×</Button>
+            <Button data={"×"} onClick={chooseOperation} operation>×</Button>
             <Button data={"4"} onClick={appendValue}>4</Button>
             <Button data={"5"} onClick={appendValue}>5</Button>
             <Button data={"6"} onClick={appendValue}>6</Button>
-            <Button operation>+</Button>
+            <Button data={"+"} onClick={chooseOperation} operation>+</Button>
             <Button data={"1"} onClick={appendValue}>1</Button>
             <Button data={"2"} onClick={appendValue}>2</Button>
             <Button data={"3"} onClick={appendValue}>3</Button>
-            <Button operation>-</Button>
+            <Button data={"-"} onClick={chooseOperation} operation>-</Button>
             <Button period  data={"."} onClick={appendValue}>.</Button>
             <Button data={"0"} onClick={appendValue}>0</Button>
-            <Button operation gridSpan={2} equals>=</Button>
+            <Button onClick={equals} operation gridSpan={2} equals>=</Button>
         </Container>
     )
 }
